d4d98407e84d819a68f73df3e63da1fdd9ef3acf
Asmial <asiermiguel@gmail.com>
2020-10-06 19:37:02 +0200

    auto scrapper (not tested)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const https = require('https')
 const Discord = require('discord.js')
 const schedule = require('node-schedule')
+const parse = require('node-html-parser')
 //const secret = require('./secret.json')
 const client = new Discord.Client();
 const fs = require('fs');
@@ -17,72 +18,110 @@ client.on('ready', () => {
  * @param {Date} startDate Start date
  */
 function fillQuests(startDate) {
-    var endDate = new Date(startDate.getTime())
-    endDate.setMonth(endDate.getMonth() + 1)
-    https.get(`https://clients6.google.com/calendar/v3/calendars/pso2.schedule@gmail.com/events?calendarId=pso2.schedule%40gmail.com&singleEvents=true&timeZone=Europe%2FMadrid&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}T00%3A00%3A00%2B02%3A00&timeMax=${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}T00%3A00%3A00%2B02%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`, (res) => {
+    https.get('https://pso2.com/news/urgent-quests', (res) => {
         var rawData = ''
         res.on('data', (chunk) => {
             rawData += chunk
         })
-        //fs.readFile('./test.json', 'utf8', (err, rawData) => {
         res.on('end', () => {
-            fs.writeFile('./res.json', rawData, 'utf8', () => { })
-            const cal = JSON.parse(rawData)
-            var i
-            for (i = 0; i < cal.items.length; i++) {
-                const event = cal.items[i];
-                var date = new Date(Date.parse(event.start.dateTime))
-                var remind = new Date(date.getTime()).setMinutes(date.getMinutes() - 30)
-                if (event["description"]) {
-                    var description = String(event["description"]).replace(/<[^>]*>|\n|\r|\ \ /gi, (x) => {
-                        switch (x[1]) {
-                            case '/':
-                                switch (x[2]) {
-                                    case 'p':
-                                        return '\n'
-                                    case 'b':
-                                        return '**'
-                                    case 'a':
-                                        return ''
-                                    default:
-                                        return x
-                                }
-                            case 'a':
-                            case 'p':
-                                return '';
-                            case 'b':
-                                switch (x[2]) {
-                                    case 'r':
-                                        return '\n'
-                                    default:
-                                        return '**'
-                                }
-                            case '\n':
-                            case '\r':
-                                return ''
-                            default:
-                                return x
+            root = parse(rawData)
+            page = root.querySelector("li.sr:nth-child(1) > div:nth-child(2) > div:nth-child(3) > a:nth-child(4)").attributes['onclick'].value.replace(/\(ShowDetails\('/gi, '').replace(/'.*/gi, '')
+            url = "https://pso2.com/news/urgent-quests/" + page
+            https.get(url, (res) => {
+                var rawData = ''
+                res.on('data', (chunk) => {
+                    rawData += chunk
+                })
+                res.on('end', () => {
+                    var root = parse(data)
+                    var iframes = root.querySelectorAll("iframe")
+                    var src = ''
+                    for (let i = 0; i < iframes.length; i++) {
+                        const iframe = iframes[i];
+                        if (iframe.src.includes("calendar.google.com")) {
+                            src = iframe.src
+                            break
                         }
-                    })
-                    description = description.replace(/\s\s|[\n|\s]+$/gi, '')
-                    console.log((i + 1) + " " + description)
-                    schedule.scheduleJob(remind, () => {
-                        sendquest(date, event["summary"], description)
-                    })
-                } else {
-                    console.log((i + 1) + " NO DESCRIPTION")
-                    schedule.scheduleJob(remind, () => {
-                        sendquest(date, event["summary"])
-                    })
-                }
-            }
-            console.log(`Scheduled ${i} events`)
+                    }
+                    if (src == '') {
+                        throw 'calendar not found'
+                    }
+                    src = src.replace(/.*&src=/gi, '').replace(/&.*/gi, '')
+                    var calendarId = Buffer.from(src, 'base64');
+                    var endDate = new Date(startDate.getTime())
+                    endDate.setMonth(endDate.getMonth() + 1)
+                    https.get(`https://clients6.google.com/calendar/v3/calendars/pso2.schedule@gmail.com/events?calendarId=${calendarId}&singleEvents=true&timeZone=Europe%2FMadrid&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}T00%3A00%3A00%2B02%3A00&timeMax=${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}T00%3A00%3A00%2B02%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`, (res) => {
+                        var rawData = ''
+                        res.on('data', (chunk) => {
+                            rawData += chunk
+                        })
+                        //fs.readFile('./test.json', 'utf8', (err, rawData) => {
+                        res.on('end', () => {
+                            fs.writeFile('./res.json', rawData, 'utf8', () => { })
+                            const cal = JSON.parse(rawData)
+                            var i
+                            for (i = 0; i < cal.items.length; i++) {
+                                const event = cal.items[i];
+                                var date = new Date(Date.parse(event.start.dateTime))
+                                var remind = new Date(date.getTime()).setMinutes(date.getMinutes() - 30)
+                                if (event["description"]) {
+                                    var description = String(event["description"]).replace(/<[^>]*>|\n|\r|\ \ /gi, (x) => {
+                                        switch (x[1]) {
+                                            case '/':
+                                                switch (x[2]) {
+                                                    case 'p':
+                                                        return '\n'
+                                                    case 'b':
+                                                        return '**'
+                                                    case 'a':
+                                                        return ''
+                                                    default:
+                                                        return x
+                                                }
+                                            case 'a':
+                                            case 'p':
+                                                return '';
+                                            case 'b':
+                                                switch (x[2]) {
+                                                    case 'r':
+                                                        return '\n'
+                                                    default:
+                                                        return '**'
+                                                }
+                                            case '\n':
+                                            case '\r':
+                                                return ''
+                                            default:
+                                                return x
+                                        }
+                                    })
+                                    description = description.replace(/\s\s|[\n|\s]+$/gi, '')
+                                    console.log((i + 1) + " " + description)
+                                    schedule.scheduleJob(remind, () => {
+                                        sendquest(date, event["summary"], description)
+                                    })
+                                } else {
+                                    console.log((i + 1) + " NO DESCRIPTION")
+                                    schedule.scheduleJob(remind, () => {
+                                        sendquest(date, event["summary"])
+                                    })
+                                }
+                            }
+                            console.log(`Scheduled ${i} events`)
 
-            var resetTime = Date.parse(cal.items[cal.items.length - 1].end.dateTime)
-            schedule.scheduleJob(resetTime, () => {
-                fillQuests(resetTime)
+                            var resetTime = Date.parse(cal.items[cal.items.length - 1].end.dateTime)
+                            schedule.scheduleJob(resetTime, () => {
+                                fillQuests(resetTime)
+                            })
+                            console.log(`Will fetch calendar again at ${cal.items[cal.items.length - 1].end.dateTime}`)
+                        })
+                    })
+
+                })
+
+
             })
-            console.log(`Will fetch calendar again at ${cal.items[cal.items.length - 1].end.dateTime}`)
+
         })
     })
 }
